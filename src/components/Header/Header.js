1e17c55743bae3ed0e303f3f8c9e1b043b7d1ce5
0alif <alifhossain003@gmail.com>
2021-10-04 06:00:00 +0600

    first commit

---
+++ src/components/Header/Header.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Navigation from '../Navigation/Navigation';
+
+const Header = () => {
+    return (
+        <div>
+            <Navigation></Navigation>
+            <h2>this is header</h2>
+        </div>
+    );
+};
+
+export default Header;
