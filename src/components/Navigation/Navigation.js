768b3cce144b203a6d2a6a9f92af7d5c1ff23b92
0alif <alifhossain003@gmail.com>
2021-10-04 07:01:19 +0600

    navigation

--- src/components/Navigation/Navigation.js
+++ src/components/Navigation/Navigation.js
@@ -1,9 +1,29 @@
 import React from 'react';
+import { Container, Nav, Navbar } from 'react-bootstrap';
+import { NavLink } from 'react-router-dom';
 
 const Navigation = () => {
     return (
         <div>
-            <h2>this is navigation</h2>
+            <Navbar bg="light" variant="light">
+                <Container>
+                    <Navbar.Brand>Learn Up</Navbar.Brand>
+                    <Nav className="me-auto">
+                        <NavLink className="me-4 text-decoration-none" to="/home" activeStyle={{
+                            fontWeight: "bold",
+                            color: "red"
+                        }}>Home</NavLink>
+                        <NavLink className="me-4 text-decoration-none" to="/about" activeStyle={{
+                            fontWeight: "bold",
+                            color: "red"
+                        }}>About</NavLink>
+                        <NavLink className="me-4 text-decoration-none" to="/services" activeStyle={{
+                            fontWeight: "bold",
+                            color: "red"
+                        }}>Services</NavLink>
+                    </Nav>
+                </Container>
+            </Navbar>
         </div>
     );
 };
